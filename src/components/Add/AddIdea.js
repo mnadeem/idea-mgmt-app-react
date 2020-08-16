8129a6506eccf462b3a1c7be16d8e53ffb5d9928
Nadeem Mohammad <coolmind182006@gmail.com>
2020-08-16 22:30:06 +0530

    Refactored

--- src/components/Add/AddIdea.js
+++ src/components/Add/AddIdea.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Icon from "../Icon/Icon";
 export default class AddIdea extends Component {
   render() {
     return (
-      <div>Add</div>
+      <React.Fragment>
+        <Container>
+          <Navbar.Brand href="#/list">
+            <Icon name="list" color="#61dafb" size={35} /> List Ideas
+          </Navbar.Brand>
+        </Container>
+        <Container>
+            
+        </Container>
+      </React.Fragment>
     );
   }
 }
--- src/components/Update/UpdateIdea.js
+++ src/components/Update/UpdateIdea.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Icon from "../Icon/Icon";
 export default class UpdateIdea extends Component {
   render() {
     return (
-      <div>Update</div>
+      <React.Fragment>
+        <Container>
+          <Navbar.Brand href="#/list">
+            <Icon name="list" color="#61dafb" size={35} /> List Ideas
+          </Navbar.Brand>
+        </Container>
+        <Container>
+            
+        </Container>
+      </React.Fragment>
     );
   }
 }
